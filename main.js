0e67db12f949f7061f6eaad1ad4c5fd1c7deaa81
dhairya-garg <82100537+dhairya-garg@users.noreply.github.com>
2022-02-11 19:37:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(300,300)
+    canvas.center();
+    video= createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+
+    posenet = ml5.poseNet(video,modelLoaded)
+    posenet.on('pose',gotPoses)
+
+}
+
+function modelLoaded(){
+    console.log("posenet is initialized")
+}
+
+function gotPoses(results){
+    if ( results.length > 0) {
+        console.log(results)
+        console.log(" canvas x ="+results[0].pose.nose.x)
+        console.log(" canvas y ="+results[0].pose.nose.y)
+    }
+
+}
+function draw(){
+image(video,0,0,300,300)
+}
+function take_snapshot(){
+    save("myFilterImage.png")
+}
+
+
